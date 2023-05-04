04a63dc18eabc053725ef37144bd0a3779d99430
Dennis Chiira <programmingchiira@gmail.com>
2023-05-04 11:53:56 +0300

    AdmYear bread

--- resources/js/pages/ecomrade/AdmYear.vue
+++ resources/js/pages/ecomrade/AdmYear.vue
@@ -13,7 +13,7 @@
 
                 <br />
                 <div class="row">
-                    <div class="col-md-4 col-9">
+                    <div class="col-md-4 col-10">
                         <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
                             <div class="container">
                                 <ol class="breadcrumb">
@@ -25,10 +25,10 @@
                         </nav>
                     </div>
 
-                    <div style="float:right;" class="col-md-6 col-4">
+                    <div style="float:right;" class="col-md-6 col-2">
                         <router-link :to="{ name: 'CreateYear' }" style="float: right;">
                             <button type="button" class="btn bg-gradient-primary btn-sm">
-                                Add
+                                <i class="fa fa-plus"></i>
                             </button>
                         </router-link>
                     </div>
