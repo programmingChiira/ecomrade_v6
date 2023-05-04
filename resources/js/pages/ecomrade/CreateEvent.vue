b93f40b84898a75cb6b369a1d68c6488967efb26
Dennis Chiira <programmingchiira@gmail.com>
2023-05-04 12:03:49 +0300

    creates breadcrumbs

--- resources/js/pages/ecomrade/CreateEvent.vue
+++ resources/js/pages/ecomrade/CreateEvent.vue
@@ -4,7 +4,7 @@
 
       <div class="container">
         <div class="row">
-          <div class="col-md-4 col-9">
+          <div class="col-md-4 col-10">
             <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
               <div class="container">
                 <ol class="breadcrumb">
