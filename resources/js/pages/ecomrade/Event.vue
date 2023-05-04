5ac8835206bf650d66595df74047e2c2abaffec7
Dennis Chiira <programmingchiira@gmail.com>
2023-05-04 12:08:48 +0300

    Edits breadcrumbs

--- resources/js/pages/ecomrade/Event.vue
+++ resources/js/pages/ecomrade/Event.vue
@@ -65,7 +65,7 @@
 
                 <div class="row">
                     <div class="row">
-                        <div class="col-md-4 col-9">
+                        <div class="col-md-4 col-10">
                             <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
                                 <div class="container">
                                     <ol class="breadcrumb">
@@ -84,11 +84,11 @@
                                 </div>
                             </nav>
                         </div>
-                        <div style="float:right;" class="col-md-8 col-3">
+                        <div style="float:right;" class="col-md-8 col-2">
                             <router-link to="/createEvent" style="float: right;" class="text-info icon-move-right">
                                 <button data-toggle="tooltip" data-placement="bottom" title="Create event"
                                     type="button" class="btn bg-gradient-primary btn-sm">
-                                    Add
+                                    <i class="fa fa-plus"></i>
                                 </button>
                             </router-link>
                         </div>
