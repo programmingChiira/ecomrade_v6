b65982ceadcf31f54fbd38e7376dfb474b5bec63
Dennis Chiira <programmingchiira@gmail.com>
2023-05-04 11:22:44 +0300

    AdmDash breadcrumbs

--- resources/js/pages/ecomrade/AdmDashboard.vue
+++ resources/js/pages/ecomrade/AdmDashboard.vue
@@ -11,7 +11,7 @@
                         <div class="container-fluid">
 
                             <div class="row">
-                                <div class="col-md-4 col-9">
+                                <div class="col-md-4 col-10">
                                     <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
                                         <div class="container">
                                             <ol class="breadcrumb">
