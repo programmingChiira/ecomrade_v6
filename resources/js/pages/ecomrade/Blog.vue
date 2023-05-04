70324a3cb186f727d4b065cdbd2e1395018fad00
Dennis Chiira <programmingchiira@gmail.com>
2023-05-04 11:54:56 +0300

    blog bread

--- resources/js/pages/ecomrade/Blog.vue
+++ resources/js/pages/ecomrade/Blog.vue
@@ -58,7 +58,7 @@
         </div>
 
         <div class="row">
-          <div class="col-md-4 col-9">
+          <div class="col-md-4 col-10">
             <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
               <div class="container">
                 <ol class="breadcrumb">
@@ -76,11 +76,11 @@
               </div>
             </nav>
           </div>
-          <div style="float:right;" class="col-md-8 col-3">
+          <div style="float:right;" class="col-md-8 col-2">
             <router-link to="/createblog" style="float: right;" class="text-info icon-move-right">
               <button data-toggle="tooltip" data-placement="bottom" title="Create blog" type="button"
                 class="btn bg-gradient-primary btn-sm">
-                Add
+                <i class="fa fa-plus"></i>
               </button>
             </router-link>
           </div>
