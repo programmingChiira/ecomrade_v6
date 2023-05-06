50009e3994a8de20b1f013d8de1749f5c9a39e7e
Dennis Chiira <programmingchiira@gmail.com>
2023-05-06 12:54:56 +0300

    getyears protec

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -70,6 +70,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/getlocations') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/getyears') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
         
         return $next($request);
     }
--- routes/api.php
+++ routes/api.php
@@ -187,8 +187,8 @@ Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/getma
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/getmarketcategories', [MarketCategoryController::class, 'getSub']);
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/getrentalcategories', [RentalCategoryController::class, 'getSub']);
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/getlocations', [LocationController::class, 'getSub']);
+Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/getyears', [YearController::class, 'getSub']);
 
-Route::middleware('throttle:1200,1')->get('/getyears',[YearController::class,'getSub']);
 Route::middleware('throttle:1200,1')->get('/getcourses',[CourseController::class,'getSub']);
 Route::middleware('throttle:1200,1')->get('/postreviews',[PostReviewController::class,'index']);
 Route::middleware('throttle:1200,1')->get('/resourcereviews',[ResourceReviewController::class,'index']);
