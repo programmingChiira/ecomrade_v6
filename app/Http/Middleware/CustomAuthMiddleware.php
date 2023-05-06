6eca5074e153d7414c5f3d4c9f4b51c8a2a9aece
Dennis Chiira <programmingchiira@gmail.com>
2023-05-06 12:30:52 +0300

    homemarketcategories protec

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -19,6 +19,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/categories') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/homemarketcategories') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
 
         return $next($request);
     }
