7bab260689b4ef1da691b4676548866ec7a4078f
Dennis Chiira <programmingchiira@gmail.com>
2023-05-06 13:44:13 +0300

    marketings protec

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -169,6 +169,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/votes') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/marketings') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
         
         return $next($request);
     }
