d490d6d0e9c6e6939f4f5f0ef02ec62d9650fbc2
Dennis Chiira <programmingchiira@gmail.com>
2023-05-06 13:32:31 +0300

    userchats protec

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -142,6 +142,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/clubchats') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/userchats') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
         
         return $next($request);
     }
--- routes/api.php
+++ routes/api.php
@@ -212,8 +212,8 @@ Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/users
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/posts', [PostController::class, 'index']);
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/clubs', [ClubController::class, 'index']);
 Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/clubchats', [ClubChatRoomController::class, 'index']);
+Route::middleware(['throttle:1200,1', CustomAuthMiddleware::class])->get('/userchats', [UserChatRoomController::class, 'index']);
 
-Route::middleware('throttle:1200,1')->get('userchats', [UserChatRoomController::class, 'index']);
 Route::middleware('throttle:1200,1')->get('resources', [ResourceController::class, 'index']);
 Route::middleware('throttle:1200,1')->get('feeds', [FeedController::class, 'index']);
 Route::middleware('throttle:1200,1')->get('messages', [MessageController::class, 'index']);
