eebdebccf3f51782d37e0f595a2b0a48301924cb
Dennis Chiira <programmingchiira@gmail.com>
2023-05-06 14:02:26 +0300

    searchuser protec

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -199,6 +199,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/searchpost') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/searchuser') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
         
         return $next($request);
     }
