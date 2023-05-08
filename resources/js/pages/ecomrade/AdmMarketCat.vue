55ef5cf8acb7d97b75f79f69ea7816c43a101e6c
Dennis Chiira <programmingchiira@gmail.com>
2023-05-08 13:15:39 +0300

    AdmMarkCat loader

--- resources/js/pages/ecomrade/AdmMarketCat.vue
+++ resources/js/pages/ecomrade/AdmMarketCat.vue
@@ -312,6 +312,7 @@ export default {
       id: "",
       name: "",
       type: "",
+      loading: true,
     };
   },
 
@@ -362,6 +363,10 @@ export default {
     },
   },
   mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
     axios
       .get("/api/user")
       .then(response => {
