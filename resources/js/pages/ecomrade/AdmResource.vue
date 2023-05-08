d25748ba167a05f9ccf76c3a4c7ca34f72db7066
Dennis Chiira <programmingchiira@gmail.com>
2023-05-08 13:24:34 +0300

    AdmResource loader

--- resources/js/pages/ecomrade/AdmResource.vue
+++ resources/js/pages/ecomrade/AdmResource.vue
@@ -419,6 +419,7 @@ export default {
       id: "",
       name: "",
       type: "",
+      loading: true,
     };
   },
 
@@ -565,6 +566,10 @@ export default {
   },
 
   mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
     axios
       .get("/api/user")
       .then(response => {
