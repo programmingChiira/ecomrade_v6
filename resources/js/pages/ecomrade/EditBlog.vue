85df406c99a4b8f89ebfcce66dfc57c3d87bb447
Dennis Chiira <programmingchiira@gmail.com>
2023-05-08 13:59:17 +0300

    EditBlog loader

--- resources/js/pages/ecomrade/EditBlog.vue
+++ resources/js/pages/ecomrade/EditBlog.vue
@@ -2,7 +2,7 @@
     <body class="index-page">
         <div class="full-page-loader" v-if="loading">
             <img src="/triangle.svg" alt="Loader" />
-          </div>
+        </div>
         <section class="my-5 py-5">
             <div class="container">
 
@@ -179,6 +179,7 @@ export default {
 
             id: "",
             name: "",
+            loading: true,
         };
     },
 
@@ -336,6 +337,9 @@ export default {
     },
 
     mounted() {
+        setTimeout(() => {
+            this.loading = false;
+        }, 2000);
 
         axios
             .get("/api/user")
