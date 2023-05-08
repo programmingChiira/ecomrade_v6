b49aee747f9ed0991f568b10a990b5b7075611c5
Dennis Chiira <programmingchiira@gmail.com>
2023-05-08 13:26:35 +0300

    AdmYear loader

--- resources/js/pages/ecomrade/AdmYear.vue
+++ resources/js/pages/ecomrade/AdmYear.vue
@@ -311,6 +311,7 @@ export default {
       id: "",
       name: "",
       type: "",
+      loading: true,
     };
   },
 
@@ -352,6 +353,10 @@ export default {
     },
   },
   mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
     axios
       .get("/api/user")
       .then(response => {
