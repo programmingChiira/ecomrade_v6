ac5754bd2f1b361669d6bce9ae72048391cbe617
Dennis Chiira <programmingchiira@gmail.com>
2023-05-08 14:01:16 +0300

    EditEvent loader

--- resources/js/pages/ecomrade/EditEvent.vue
+++ resources/js/pages/ecomrade/EditEvent.vue
@@ -2,7 +2,7 @@
     <body class="index-page">
         <div class="full-page-loader" v-if="loading">
             <img src="/triangle.svg" alt="Loader" />
-          </div>
+        </div>
         <section class="my-5 py-5">
 
             <div class="container">
@@ -304,6 +304,7 @@ export default {
             links: [],
             phone: "",
             locations: {},
+            loading: true,
 
             // id: "",
             // name: "",
@@ -615,6 +616,9 @@ export default {
     },
 
     mounted() {
+        setTimeout(() => {
+            this.loading = false;
+        }, 2000);
 
         axios
             .get("/api/user")
