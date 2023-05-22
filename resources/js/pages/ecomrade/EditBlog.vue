9bab9eb7f785eb354a89e3f133bbe6a0cfe7223e
Dennis Chiira <programmingchiira@gmail.com>
2023-05-22 16:05:11 +0300

    editBlog

--- resources/js/pages/ecomrade/EditBlog.vue
+++ resources/js/pages/ecomrade/EditBlog.vue
@@ -5,109 +5,196 @@
         </div>
         <section class="my-5 py-5">
             <div class="container">
-
                 <div class="row">
                     <div class="col-md-4 col-10">
-                        <nav aria-label="breadcrumb" class="breadcrumb-nav mb-2">
+                        <nav
+                            aria-label="breadcrumb"
+                            class="breadcrumb-nav mb-2"
+                        >
                             <div class="container">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><router-link to="/">Home</router-link></li>
-                                    <li class="breadcrumb-item"><router-link to="/blog">Blog</router-link></li>
-                                    <li style="color: #189483;" class="breadcrumb-item"><strong>Edit</strong></li>
+                                    <li class="breadcrumb-item">
+                                        <router-link to="/">Home</router-link>
+                                    </li>
+                                    <li class="breadcrumb-item">
+                                        <router-link to="/blog"
+                                            >Blog</router-link
+                                        >
+                                    </li>
+                                    <li
+                                        style="color: #189483"
+                                        class="breadcrumb-item"
+                                    >
+                                        <strong>Edit</strong>
+                                    </li>
                                 </ol>
                             </div>
                         </nav>
                     </div>
                 </div>
 
-                <div class="row align-items-center  justify-content-center">
-
-                    <div style="padding: 10px;" class="col-lg-12 col-sm-12">
+                <div class="row align-items-center justify-content-center">
+                    <div style="padding: 10px" class="col-lg-12 col-sm-12">
                         <form @submit.prevent="submit">
                             <!-- Title -->
                             <label for="title"><span>Title</span></label>
-                            <input class="form-control" type="text" id="title" v-model="fields.title" />
-                            <span v-if="errors.title" class="error">{{ errors.title[0] }}</span>
+                            <input
+                                class="form-control"
+                                type="text"
+                                id="title"
+                                v-model="fields.title"
+                            />
+                            <span v-if="errors.title" class="error">{{
+                                errors.title[0]
+                            }}</span>
                             <br />
 
                             <!-- Image -->
                             <label for="image"><span>Image 1</span></label>
-                            <input class="form-control" type="file" id="image" @input="grabFile1" />
-                            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+                            <input
+                                class="form-control"
+                                type="file"
+                                id="image"
+                                @input="grabFile1"
+                            />
+                            <span v-if="errors.file" class="error">{{
+                                errors.file[0]
+                            }}</span>
                             <div class="preview row">
-                                <div class="col-6 col-md-6">
-
-                                </div>
+                                <div class="col-6 col-md-6"></div>
                                 <img :src="url1" alt="" />
                             </div>
                             <br />
 
-                            <label v-if="input2 === 2" for="image"><span>Image 2</span></label>
-                            <input v-if="input2 === 2" class="form-control" type="file" id="image" @input="grabFile2" />
-                            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+                            <label v-if="input2 === 2" for="image"
+                                ><span>Image 2</span></label
+                            >
+                            <input
+                                v-if="input2 === 2"
+                                class="form-control"
+                                type="file"
+                                id="image"
+                                @input="grabFile2"
+                            />
+                            <span v-if="errors.file" class="error">{{
+                                errors.file[0]
+                            }}</span>
                             <div v-if="input2 === 2" class="preview row">
-                                <div class="col-6 col-md-6">
-
-                                </div>
+                                <div class="col-6 col-md-6"></div>
                                 <img :src="url2" alt="" />
                             </div>
                             <hr />
 
-                            <label v-if="input3 === 3" for="image"><span>Image 3</span></label>
-                            <input v-if="input3 === 3" class="form-control" type="file" id="image" @input="grabFile3" />
-                            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+                            <label v-if="input3 === 3" for="image"
+                                ><span>Image 3</span></label
+                            >
+                            <input
+                                v-if="input3 === 3"
+                                class="form-control"
+                                type="file"
+                                id="image"
+                                @input="grabFile3"
+                            />
+                            <span v-if="errors.file" class="error">{{
+                                errors.file[0]
+                            }}</span>
                             <div v-if="input3 === 3" class="preview row">
-                                <div class="col-6 col-md-6">
-
-                                </div>
+                                <div class="col-6 col-md-6"></div>
                                 <img :src="url3" alt="" />
                             </div>
                             <hr />
 
-                            <label v-if="input4 === 4" for="image"><span>Image 4</span></label>
-                            <input v-if="input4 === 4" class="form-control" type="file" id="image" @input="grabFile4" />
-                            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+                            <label v-if="input4 === 4" for="image"
+                                ><span>Image 4</span></label
+                            >
+                            <input
+                                v-if="input4 === 4"
+                                class="form-control"
+                                type="file"
+                                id="image"
+                                @input="grabFile4"
+                            />
+                            <span v-if="errors.file" class="error">{{
+                                errors.file[0]
+                            }}</span>
                             <div v-if="input4 === 4" class="preview row">
-                                <div class="col-6 col-md-6">
-
-                                </div>
+                                <div class="col-6 col-md-6"></div>
                                 <img :src="url4" alt="" />
                             </div>
                             <hr />
 
-                            <label v-if="input5 === 5" for="image"><span>Image 5</span></label>
-                            <input v-if="input5 === 5" class="form-control" type="file" id="image" @input="grabFile5" />
-                            <span v-if="errors.file" class="error">{{ errors.file[0] }}</span>
+                            <label v-if="input5 === 5" for="image"
+                                ><span>Image 5</span></label
+                            >
+                            <input
+                                v-if="input5 === 5"
+                                class="form-control"
+                                type="file"
+                                id="image"
+                                @input="grabFile5"
+                            />
+                            <span v-if="errors.file" class="error">{{
+                                errors.file[0]
+                            }}</span>
                             <div v-if="input5 === 5" class="preview row">
-                                <div class="col-6 col-md-6">
-
-                                </div>
+                                <div class="col-6 col-md-6"></div>
                                 <img :src="url5" alt="" />
                             </div>
                             <hr />
 
-                            <h4 v-if="input6 === 6" style="color: #189483;"> Sorry ! you've maxed out on image upload.
+                            <h4 v-if="input6 === 6" style="color: #189483">
+                                Sorry ! you've maxed out on image upload.
                             </h4>
 
-                            <button class="btn bg-gradient-primary btn-sm" v-if="disappear2" type="button"
-                                @click="showInput2">Add
-                                Image </button>
-                            <button class="btn bg-gradient-primary btn-sm" v-if="disappear3" type="button"
-                                @click="showInput3">Add
-                                Image </button>
-                            <button class="btn bg-gradient-primary btn-sm" v-if="disappear4" type="button"
-                                @click="showInput4">Add
-                                Image </button>
-                            <button class="btn bg-gradient-primary btn-sm" v-if="disappear5" type="button"
-                                @click="showInput5">Add
-                                Image </button>
+                            <button
+                                class="btn bg-gradient-primary btn-sm"
+                                v-if="disappear2"
+                                type="button"
+                                @click="showInput2"
+                            >
+                                Add Image
+                            </button>
+                            <button
+                                class="btn bg-gradient-primary btn-sm"
+                                v-if="disappear3"
+                                type="button"
+                                @click="showInput3"
+                            >
+                                Add Image
+                            </button>
+                            <button
+                                class="btn bg-gradient-primary btn-sm"
+                                v-if="disappear4"
+                                type="button"
+                                @click="showInput4"
+                            >
+                                Add Image
+                            </button>
+                            <button
+                                class="btn bg-gradient-primary btn-sm"
+                                v-if="disappear5"
+                                type="button"
+                                @click="showInput5"
+                            >
+                                Add Image
+                            </button>
                             <br />
 
                             <!-- Drop down -->
-                            <label for="categories"><span>Choose a category:</span></label>
-                            <select class="form-control" v-model="fields.post_category" id="categories">
+                            <label for="categories"
+                                ><span>Choose a category:</span></label
+                            >
+                            <select
+                                class="form-control"
+                                v-model="fields.post_category"
+                                id="categories"
+                            >
                                 <option disabled value="">Select option</option>
-                                <option :value="category.name" v-for="category in categories" :key="category.id">
+                                <option
+                                    :value="category.name"
+                                    v-for="category in categories"
+                                    :key="category.id"
+                                >
                                     {{ category.name }}
                                 </option>
                             </select>
@@ -118,34 +205,57 @@
 
                             <!-- Body-->
                             <label for="body"><span>Body</span></label>
-                            <textarea class="form-control" id="body" v-model="fields.body" rows="6"></textarea>
-                            <span v-if="errors.body" class="error">{{ errors.body[0] }}</span>
-                            <br>
+                            <textarea
+                                class="form-control"
+                                id="body"
+                                v-model="fields.body"
+                                rows="6"
+                            ></textarea>
+                            <span v-if="errors.body" class="error">{{
+                                errors.body[0]
+                            }}</span>
+                            <br />
                             <!-- Button -->
-                            <button style="margin: 5px;float:right;" class="btn bg-gradient-primary btn-sm"
-                                type="submit">Submit</button>
+                            <button
+                                v-if="!isSubmitting"
+                                style="margin: 5px; float: right"
+                                class="btn bg-gradient-primary btn-sm"
+                                type="submit"
+                            >
+                                Submit
+                            </button>
+                            <i
+                                v-if="isSubmitting"
+                                style="
+                                    -webkit-animation: fa-spin 3s infinite
+                                        linear;
+                                    animation: fa-spin 2s infinite linear;
+                                    font-size: 17px;
+                                    color: #189483;
+                                    float: right;
+                                "
+                                class="fa fa-graduation-cap"
+                            ></i>
                         </form>
                     </div>
                 </div>
             </div>
-            <hr class="horizontal dark my-5">
+            <hr class="horizontal dark my-5" />
         </section>
         <Footer />
     </body>
 </template>
 
 <script>
-import Footer from './Footer.vue'
+import Footer from "./Footer.vue";
 
 export default {
-
     components: {
-        Footer
+        Footer,
     },
     props: ["slug"],
     data() {
         return {
-
             showInput: 0,
 
             disappear2: true,
@@ -180,11 +290,11 @@ export default {
             id: "",
             name: "",
             loading: true,
+            isSubmitting: false,
         };
     },
 
     methods: {
-
         showInput2() {
             this.disappear2 = false;
             this.disappear3 = true;
@@ -250,21 +360,22 @@ export default {
         },
 
         submit() {
-            let timerInterval
+            this.isSubmitting = true;
+            let timerInterval;
             Swal.fire({
-                title: 'Processing',
-                html: '',
+                title: "Processing",
+                html: "",
                 timer: 1000,
                 timerProgressBar: true,
 
                 didOpen: () => {
-                    Swal.showLoading()
-                    const b = Swal.getHtmlContainer().querySelector('b')
+                    Swal.showLoading();
+                    const b = Swal.getHtmlContainer().querySelector("b");
                     timerInterval = setInterval(() => {
                         if (b) {
-                            b.textContent = Swal.getTimerLeft()
+                            b.textContent = Swal.getTimerLeft();
                         }
-                    }, 100)
+                    }, 100);
 
                     const fd = new FormData();
                     fd.append("title", this.fields.title);
@@ -300,22 +411,30 @@ export default {
                         .catch((error) => {
                             this.errors = error.response.data.errors;
                             if (error.response.status === 403) {
-                                this.$router.push({ name: "DashboardPostsList" });
+                                this.$router.push({
+                                    name: "DashboardPostsList",
+                                });
                             }
                         });
                 },
                 willClose: () => {
-                    clearInterval(timerInterval)
-                }
-            }).then((result) => {
-                /* Read more about handling dismissals below */
-                if (result.dismiss === Swal.DismissReason.timer) {
-                    console.log('All is well')
-                    this.$router.push({ name: "Blog" });
-                }
+                    clearInterval(timerInterval);
+                },
             })
+                .then((result) => {
+                    /* Read more about handling dismissals below */
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        this.isSubmitting = true;
+                        console.log("All is well");
+                        this.$router.push({ name: "Blog" });
+                        this.isSubmitting = true;
+                    }
+                })
+                .catch((error) => {
+                    this.errors = error.response.data.errors;
+                    this.isSubmitting = false;
+                });
         },
-
     },
 
     watch: {
@@ -374,7 +493,6 @@ export default {
             .catch((error) => {
                 console.log(error);
             });
-
     },
 };
 </script>
