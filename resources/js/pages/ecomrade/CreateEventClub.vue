9828d61f3ed6d5e330e5741b11d9c5e1811197f1
Dennis Chiira <programmingchiira@gmail.com>
2023-05-22 15:08:54 +0300

    CrEventClub

--- resources/js/pages/ecomrade/CreateEventClub.vue
+++ resources/js/pages/ecomrade/CreateEventClub.vue
@@ -220,8 +220,26 @@
                                 <span v-if="errors.mode" class="error">{{ errors.mode[0] }}</span>
                                 <br />
                                 <button class="btn btn-sm" @click.prevent="prevStep">Prev </button>
-                                <button style="margin: 5px;float:right;" class="btn bg-gradient-primary btn-sm"
-                                    type="submit">Submit</button>
+                                <button
+                                v-if="!isSubmitting"
+                                style="margin: 5px; float: right"
+                                class="btn bg-gradient-primary btn-sm"
+                                type="submit"
+                            >
+                                Submit
+                            </button>
+                            <i
+                                v-if="isSubmitting"
+                                style="
+                                    -webkit-animation: fa-spin 3s infinite
+                                        linear;
+                                    animation: fa-spin 2s infinite linear;
+                                    font-size: 17px;
+                                    color: #189483;
+                                    float: right;
+                                "
+                                class="fa fa-graduation-cap"
+                            ></i>
                             </div>
                         </form>
                     </div>
@@ -281,6 +299,7 @@ export default {
             id: "",
             name: "",
             loading: true,
+            isSubmitting: false,
         };
     },
     computed: {
@@ -523,6 +542,7 @@ export default {
         },
 
         submit() {
+            this.isSubmitting = true;
             const fd = new FormData();
             fd.append("title", this.fields.title);
             fd.append("file1", this.fields.file1);
@@ -591,8 +611,10 @@ export default {
                     }).then((result) => {
                         /* Read more about handling dismissals below */
                         if (result.dismiss === Swal.DismissReason.timer) {
+                            this.isSubmitting = true;
                             console.log('All is well')
                             this.$router.push({ name: "Event" });
+                            this.isSubmitting = true;
                         }
                     })
 
@@ -603,6 +625,7 @@ export default {
                 .catch((error) => {
                     this.errors = error.response.data.errors;
                     this.success = false;
+                    this.isSubmitting = false;
                 });
         },
 
