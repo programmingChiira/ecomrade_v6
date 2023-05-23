83d762e7cefe7c61b418ce75d35421d9ea59a7ee
Dennis Chiira <programmingchiira@gmail.com>
2023-05-23 22:10:15 +0300

    MktCount custom

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -13,6 +13,9 @@ class CustomAuthMiddleware
         if (Str::contains($request->getRequestUri(), '/api/markets') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
+        if (Str::contains($request->getRequestUri(), '/api/marketCount') && !$request->headers->has('referer')) {
+            abort(500, 'Server Error');
+        }
         if (Str::contains($request->getRequestUri(), '/api/connections') && !$request->headers->has('referer')) {
             abort(500, 'Server Error');
         }
