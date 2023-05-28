0540f1473b3d5ff0264f5212f29447bb5317bb47
Dennis Chiira <programmingchiira@gmail.com>
2023-05-28 21:30:48 +0300

    AdmBlog loader

--- resources/js/pages/ecomrade/AdmBlog.vue
+++ resources/js/pages/ecomrade/AdmBlog.vue
@@ -1,8 +1,6 @@
 <template>
   <body class="about-us">
-    <div class="full-page-loader" v-if="loading">
-      <img src="/triangle.svg" alt="Loader" />
-    </div>
+    <Loader/>
     <section class="my-5 py-5">
       <div v-if="type == 'admin'" class="container">
 
@@ -358,15 +356,15 @@
 
 <script>
 import Footer from './Footer.vue'
-
 import axios from 'axios';
-
 import Cookies from 'js-cookie';
+import Loader from './Loader.vue'
 
 export default {
 
   components: {
-    Footer
+    Footer,
+    Loader
   },
   emits: ["updateSidebar"],
   data() {
@@ -393,7 +391,6 @@ export default {
       id: "",
       name: "",
       type: "",
-      loading: true,
     };
   },
 
@@ -534,10 +531,6 @@ export default {
   },
 
   mounted() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 2000);
-
     axios
       .get("/api/user")
       .then(response => {
