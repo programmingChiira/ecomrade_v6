a6e7b0aaf9ef56d5c95089f6589765f04aabc67d
Dennis Chiira <programmingchiira@gmail.com>
2023-05-28 21:39:27 +0300

    Admevent load

--- resources/js/pages/ecomrade/AdmEvent.vue
+++ resources/js/pages/ecomrade/AdmEvent.vue
@@ -1,8 +1,6 @@
 <template>
   <body class="about-us">
-    <div class="full-page-loader" v-if="loading">
-      <img src="/triangle.svg" alt="Loader" />
-    </div>
+    <Loader/>
     <section class="my-5 py-5">
 
       <div v-if="type == 'admin'" class="container">
@@ -384,7 +382,7 @@
 
 <script>
 import Footer from './Footer.vue'
-
+import Loader from './Loader.vue'
 import axios from 'axios';
 
 import Cookies from 'js-cookie';
@@ -425,7 +423,6 @@ export default {
       name: "",
       type: "",
       currentDateValue: new Date(),
-      loading: true,
     };
   },
 
@@ -572,10 +569,6 @@ export default {
   },
 
   mounted() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 2000);
-
     axios
       .get("/api/user")
       .then(response => {
