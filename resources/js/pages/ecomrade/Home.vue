e1b9361a4fff8bee15ad222360bc10a7189a6892
Dennis Chiira <programmingchiira@gmail.com>
2023-06-02 23:00:13 +0300

    xcjknjk

--- resources/js/pages/ecomrade/Home.vue
+++ resources/js/pages/ecomrade/Home.vue
@@ -132,6 +132,56 @@
           </div>
         </div>
 
+        <div class="container mt-5">
+
+
+    	<div class="row">
+
+    		<div class="col-md-3">
+
+    				<div class="card">
+
+    					<div class="image-container">
+
+    						<div class="first">
+    							<div class="d-flex justify-content-between align-items-center">
+    							<span class="discount">-25%</span>
+    							<span class="wishlist"><i class="fa fa-heart-o"></i></span>
+    						    </div>
+    						</div>
+    						<img src="https://ecomrade.co.ke/assets/img/logo.png" class="img-fluid rounded thumbnail-image">
+    					</div>
+
+    					<div class="product-detail-container p-2">
+    							<div class="d-flex justify-content-between align-items-center">
+    								<h5 class="dress-name">White traditional long dress</h5>
+    								<div class="d-flex flex-column mb-2">
+    									<span class="new-price">$3.99</span>
+    									<small class="old-price text-right">$5.99</small>
+    								</div>
+    							</div>
+    							<div class="d-flex justify-content-between align-items-center pt-1">
+    								<div class="color-select d-flex ">
+
+    									<input type="button" name="grey" class="btn creme">
+    									<input type="button" name="red" class="btn red ml-2">
+    									<input type="button" name="blue" class="btn blue ml-2">
+
+    								</div>
+    							</div>
+    							<div class="d-flex justify-content-between align-items-center pt-1">
+    								<div>
+    									<i class="fa fa-star-o rating-star"></i>
+    									<span class="rating-number">4.8</span>
+    								</div>
+    								<span class="buy">BUY +</span>
+    							</div>
+    					</div>
+    				</div>
+	    		</div>
+    	</div>
+    </div>
+
         <div class="row justify-content-center">
           <div class="col-lg-4 col-sm-6 col-12" v-for="market in markets" :key="market.id">
             <div class="card card-plain card-blog">
@@ -1074,5 +1124,88 @@ export default {
   display: inline-block;
   width: 27px;
   height: 27px;
+}
+
+
+
+
+.card{
+	background-color: #fff;
+	border:none;
+	border-radius: 10px;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.image-container{
+	position: relative;
+}
+
+.thumbnail-image{
+	border-radius: 10px !important;
+}
+
+.discount{
+	background-color: red;
+	padding-top: 1px;
+	padding-bottom: 1px;
+	padding-left: 4px;
+	padding-right: 4px;
+	font-size: 10px;
+	border-radius: 6px;
+	color: #fff;
+}
+
+.wishlist{
+	height: 25px;
+	width: 25px;
+	background-color: #eee;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 50%;
+	box-shadow:  0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.first{
+	position: absolute;
+	width: 100%;
+	    padding: 9px;
+}
+
+.dress-name{
+	font-size: 13px;
+	font-weight: bold;
+	width: 75%;
+}
+
+.new-price{
+	font-size: 13px;
+	font-weight: bold;
+	color: red;
+	
+}
+.old-price{
+	font-size: 8px;
+	font-weight: bold;
+	color: grey;
+}
+
+.creme{
+	background-color: #fff;
+	border: 2px solid grey;
+}
+.creme:hover {
+	border: 3px solid grey;
+}
+
+.creme:focus {
+	background-color: grey;
+
+}
+
+.red{
+	background-color: #fff;
+	border: 2px solid red;
+	
 }
 </style>
