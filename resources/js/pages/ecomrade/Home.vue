7bcd172c92fab1582380fa6985c1ad2c7ac876ac
programmingChiira <programmingchiira@gmail.com>
2023-06-05 00:52:19 +0300

    home.vue icones removed

--- resources/js/pages/ecomrade/Home.vue
+++ resources/js/pages/ecomrade/Home.vue
@@ -618,32 +618,6 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="info-horizontal bg-gradient-primary border-radius-xl p-5">
-            <div class="icon">
-              <svg class="text-white" width="25px" height="25px" viewBox="0 0 46 43" version="1.1"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <title>delivery-fast</title>
-                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g transform="translate(-2320.000000, -741.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                    <g transform="translate(1716.000000, 291.000000)">
-                      <g id="delivery-fast" transform="translate(604.000000, 450.000000)">
-                        <rect class="color-background" opacity="0.601143973" x="0" y="0" width="17.25"
-                          height="3.83333333"></rect>
-                        <rect class="color-background" opacity="0.601143973" x="3.83333333" y="7.66666667"
-                          width="13.4166667" height="3.83333333"></rect>
-                        <rect class="color-background" opacity="0.601143973" x="7.66666667" y="15.3333333"
-                          width="9.58333333" height="3.83333333"></rect>
-                        <rect class="color-background" opacity="0.601888021" x="11.5" y="23" width="5.75"
-                          height="3.83333333"></rect>
-                        <path class="color-foreground"
-                          d="M44.9400833,19.3679167 L38.0611667,15.9294167 L36.3591667,9.1195 C36.1464167,8.26466667 35.37975,7.66666667 34.5,7.66666667 L31.3854167,7.66666667 L21.0833333,7.66666667 C21.0833333,7.66666667 21.0833333,31.5310833 21.0833333,32.5833333 C21.0833333,33.6355833 21.1810833,34.5 21.1810833,34.5 C21.6640833,38.801 25.2808333,42.1666667 29.7083333,42.1666667 C34.1358333,42.1666667 37.7525833,38.801 38.2355833,34.5 L44.0833333,34.5 C45.1413333,34.5 46,33.6413333 46,32.5833333 L46,21.0833333 C46,20.3569167 45.5898333,19.69375 44.9400833,19.3679167 Z M29.7083333,38.3333333 C27.0671667,38.3333333 24.9166667,36.18475 24.9166667,33.5416667 C24.9166667,30.8985833 27.0671667,28.75 29.7083333,28.75 C32.3495,28.75 34.5,30.8985833 34.5,33.5416667 C34.5,36.18475 32.3495,38.3333333 29.7083333,38.3333333 Z M24.9166667,17.25 L24.9166667,11.5 L33.2426667,11.5 L34.5,17.25 L24.9166667,17.25 Z">
-                        </path>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </div>
-
             <div class="description ps-5">
               <h5 class="text-white">Getting Started</h5>
               <p style="color: white;">Have your voice heard on ecomrade by creating anonymous polls.
@@ -658,26 +632,6 @@
         </div>
         <div class="col-lg-4 px-lg-1 mt-lg-0 mt-4">
           <div class="info-horizontal bg-gray-100 border-radius-xl p-5">
-            <div class="icon">
-              <svg class="text-primary" width="25px" height="25px" viewBox="0 0 40 44" version="1.1"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <title>document</title>
-                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g transform="translate(-1870.000000, -591.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                    <g transform="translate(1716.000000, 291.000000)">
-                      <g transform="translate(154.000000, 300.000000)">
-                        <path class="color-foreground"
-                          d="M40,40 L36.3636364,40 L36.3636364,3.63636364 L5.45454545,3.63636364 L5.45454545,0 L38.1818182,0 C39.1854545,0 40,0.814545455 40,1.81818182 L40,40 Z"
-                          opacity="0.603585379"></path>
-                        <path class="color-background"
-                          d="M30.9090909,7.27272727 L1.81818182,7.27272727 C0.814545455,7.27272727 0,8.08727273 0,9.09090909 L0,41.8181818 C0,42.8218182 0.814545455,43.6363636 1.81818182,43.6363636 L30.9090909,43.6363636 C31.9127273,43.6363636 32.7272727,42.8218182 32.7272727,41.8181818 L32.7272727,9.09090909 C32.7272727,8.08727273 31.9127273,7.27272727 30.9090909,7.27272727 Z M18.1818182,34.5454545 L7.27272727,34.5454545 L7.27272727,30.9090909 L18.1818182,30.9090909 L18.1818182,34.5454545 Z M25.4545455,27.2727273 L7.27272727,27.2727273 L7.27272727,23.6363636 L25.4545455,23.6363636 L25.4545455,27.2727273 Z M25.4545455,20 L7.27272727,20 L7.27272727,16.3636364 L25.4545455,16.3636364 L25.4545455,20 Z">
-                        </path>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </div>
             <div class="description ps-5">
               <h5>Plugins</h5>
               <p>With ecomrade, you can vote on polls and have your say in important
@@ -693,26 +647,6 @@
 
         <div class="col-lg-4 mt-lg-0 mt-4">
           <div class="info-horizontal bg-gray-100 border-radius-xl p-5">
-            <div class="icon">
-              <svg class="text-primary" width="25px" height="25px" viewBox="0 0 40 40" version="1.1"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <title>ungroup</title>
-                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                  <g transform="translate(-2170.000000, -442.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                    <g transform="translate(1716.000000, 291.000000)">
-                      <g id="ungroup" transform="translate(454.000000, 151.000000)">
-                        <path class="color-background"
-                          d="M38.1818182,10.9090909 L32.7272727,10.9090909 L32.7272727,30.9090909 C32.7272727,31.9127273 31.9127273,32.7272727 30.9090909,32.7272727 L10.9090909,32.7272727 L10.9090909,38.1818182 C10.9090909,39.1854545 11.7236364,40 12.7272727,40 L38.1818182,40 C39.1854545,40 40,39.1854545 40,38.1818182 L40,12.7272727 C40,11.7236364 39.1854545,10.9090909 38.1818182,10.9090909 Z">
-                        </path>
-                        <path class="color-foreground"
-                          d="M27.2727273,29.0909091 L1.81818182,29.0909091 C0.812727273,29.0909091 0,28.2781818 0,27.2727273 L0,1.81818182 C0,0.814545455 0.812727273,0 1.81818182,0 L27.2727273,0 C28.2781818,0 29.0909091,0.814545455 29.0909091,1.81818182 L29.0909091,27.2727273 C29.0909091,28.2781818 28.2781818,29.0909091 27.2727273,29.0909091 Z">
-                        </path>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
-            </div>
             <div class="description ps-5">
               <h5>Utility Classes</h5>
               <p>Get involved in campus issues with ecomrade's anonymous polls. See the results
