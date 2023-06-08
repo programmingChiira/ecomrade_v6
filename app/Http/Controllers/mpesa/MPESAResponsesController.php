1468b5b3fe5f3fec006c2322f19f9f4b596e0c1d
programmingChiira <programmingchiira@gmail.com>
2023-06-08 15:35:27 +0300

    Controller [app/Http/Controllers/mpesa/MPESAResponsesController.php] created successfully.

---
+++ app/Http/Controllers/mpesa/MPESAResponsesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\mpesa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MPESAResponsesController extends Controller
+{
+    //
+}
