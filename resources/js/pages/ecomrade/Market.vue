80b280ced86ad9d6f09df8f805315d127b2452c4
programmingChiira <programmingchiira@gmail.com>
2023-07-08 21:12:39 +0300

    locations

--- resources/js/pages/ecomrade/Market.vue
+++ resources/js/pages/ecomrade/Market.vue
@@ -89,7 +89,7 @@
                                             <ul>
                                                 <li style="display: inline;" v-for="location in locations"
                                                     :key="location.id">
-                                                    <input data-bs-dismiss="modal" type="button"
+                                                    <input style="margin: 3px;" data-bs-dismiss="modal" type="button"
                                                         name="search" :value="location.name"
                                                         @click="product_name = location.name">
                                                 </li>
