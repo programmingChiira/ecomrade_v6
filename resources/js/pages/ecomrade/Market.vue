169e082e4bb4f36176d0bed391a40ccd394aa1ea
programmingChiira <programmingchiira@gmail.com>
2023-07-08 21:12:04 +0300

    market margin

--- resources/js/pages/ecomrade/Market.vue
+++ resources/js/pages/ecomrade/Market.vue
@@ -58,7 +58,7 @@
                                             <ul>
                                                 <li style="display: inline;" v-for="category in categories"
                                                     :key="category.id">
-                                                    <input data-bs-dismiss="modal" type="button"
+                                                    <input style="margin: 3px;" data-bs-dismiss="modal" type="button"
                                                         name="search" :value="category.name"
                                                         @click="product_name = category.name">
                                                 </li>
